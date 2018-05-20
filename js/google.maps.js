d9c5a90dd00c80ecebd14290c34d8e266e8456e0
raetzman <raetzman@gmx.de>
2018-05-20 23:08:57 +0200

    layout

---
+++ js/google.maps.js
@@ -0,0 +1,8 @@
+var map;
+      function initMap() {
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 48.7758459, lng: 9.182932100000016},
+          zoom: 13
+        });
+      }
